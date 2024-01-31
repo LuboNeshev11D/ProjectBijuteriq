06a73a188ab257d1c13157d290ca44bf1774a77c
LuboNeshev11D <toshkotashaka@gmail.com>
2024-01-31 09:20:59 +0200

    firstcommit

---
+++ Bijuteriq/Bijuteriq/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bijuteriq.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<Client>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+    }
+}
---
+++ Bijuteriq/Bijuteriq/Data/Client.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Bijuteriq.Data
+{
+    public class Client:IdentityUser
+    {
+        public string FirstName { get; set; }   
+        public string LastName { get; set; } 
+        public string Adress { get; set; }
+        public DateTime RegDate { get; set; }
+        public ICollection<Order> Orders { get; set; }
+    }
+}
