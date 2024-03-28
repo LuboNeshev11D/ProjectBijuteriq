88d0b94441c718a408f552dc67e617a473669f3b
LuboNeshev11D <toshkotashaka@gmail.com>
2024-03-28 18:24:20 +0200

    BEZ REGISTER

---
+++ Bijuteriq/Data/Order.cs
@@ -0,0 +1,13 @@
+﻿namespace Bijuteriq.Data
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int ProductID { get; set; }//F.K M:1
+        public Product Products { get; set; }//M:1
+        public string ClientID { get; set; }//F.K M:1
+        public Client Clients { get; set; }//M:1 
+        public int Quantity { get; set; }
+        public DateTime OrderOn { get; set; }
+    }
+}
